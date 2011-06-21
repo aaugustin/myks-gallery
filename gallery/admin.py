39517a05b134218bf445d32c85d0ee6de9a115bc
Aymeric Augustin <aymeric.augustin@m4x.org>
2011-06-21 22:27:38 +0200

    Optimize admin.

--- gallery/admin.py
+++ gallery/admin.py
@@ -6,12 +6,21 @@ from .models import Album, Photo
 
 
 class AlbumAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('dirpath', 'date', 'name')
     ordering = ('-date',)
+    readonly_fields = ('dirpath',)
+    search_fields = ('name', 'dirpath')
 
 admin.site.register(Album, AlbumAdmin)
 
 
 class PhotoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('filename', 'date')
     ordering = ('date',)
+    readonly_fields = ('filename',)
+    search_fields = ('album__name', 'filename')
 
 admin.site.register(Photo, PhotoAdmin)
+
--- gallery/models.py
+++ gallery/models.py
@@ -6,12 +6,29 @@ from django.db import models
 
 
 class Album(models.Model):
-    dirpath = models.CharField(max_length=200, unique=True)
+    dirpath = models.CharField(max_length=200, unique=True,
+            verbose_name="directory path")
     date = models.DateField(null=True, blank=True)
     name = models.CharField(max_length=100, blank=True)
+
+    def __unicode__(self):
+        return self.name or self.dirpath
+
+    def get_absolute_url(self):
+        raise NotImplementedError
 
 
 class Photo(models.Model):
     album = models.ForeignKey(Album)
-    filename = models.CharField(max_length=100)
+    filename = models.CharField(max_length=100,
+        verbose_name="file name")
     date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('album', 'filename')
+
+    def __unicode__(self):
+        return self.filename
+
+    def get_absolute_url(self):
+        raise NotImplementedError
