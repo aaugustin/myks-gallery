90d7d5deabf9e55ce75da06a61088630c2a2d103
Aymeric Augustin <aymeric.augustin@m4x.org>
2012-05-20 11:23:32 +0200

    Remove blanket protection in preparation for granular access control.

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,17 +2,15 @@
 # Copyright (c) 2011-2012 Aymeric Augustin. All rights reserved.
 
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import permission_required
 
 from . import views
 
-protect = permission_required('gallery.view')
 
 urlpatterns = patterns('',
-    url(r'^$', protect(views.GalleryIndexView.as_view()), name='gallery-index'),
-    url(r'^year/(?P<year>\d{4})/$', protect(views.GalleryYearView.as_view()), name='gallery-year'),
-    url(r'^album/(?P<pk>\d+)/$', protect(views.AlbumView.as_view()), name='gallery-album'),
-    url(r'^photo/(?P<pk>\d+)/$', protect(views.PhotoView.as_view()), name='gallery-photo'),
-    url(r'^original/(?P<pk>\d+)/$', protect(views.original_photo), name='gallery-photo-original'),
-    url(r'^(?P<preset>\w+)/(?P<pk>\d+)/$', protect(views.resized_photo), name='gallery-photo-resized'),
+    url(r'^$', views.GalleryIndexView.as_view(), name='gallery-index'),
+    url(r'^year/(?P<year>\d{4})/$', views.GalleryYearView.as_view(), name='gallery-year'),
+    url(r'^album/(?P<pk>\d+)/$', views.AlbumView.as_view(), name='gallery-album'),
+    url(r'^photo/(?P<pk>\d+)/$', views.PhotoView.as_view(), name='gallery-photo'),
+    url(r'^original/(?P<pk>\d+)/$', views.original_photo, name='gallery-photo-original'),
+    url(r'^(?P<preset>\w+)/(?P<pk>\d+)/$', views.resized_photo, name='gallery-photo-resized'),
 )
