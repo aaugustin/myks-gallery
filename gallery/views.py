aaugustin/myks-gallery

commit e82efd14b8e288b6747788931180639991738e1f

Rename settings.

gallery/views.py
@@ -28,11 +28,6 @@ class GalleryCommonMixin(object):
             self._can_view_all = self.request.user.has_perm('gallery.view')
         return self._can_view_all
 
-    def get_context_data(self, **kwargs):
-        context = super(GalleryCommonMixin, self).get_context_data(**kwargs)
-        context['title'] = getattr(settings, 'PHOTO_TITLE', u"Gallery")
-        return context
-
 
 class AlbumListMixin(object):
     """Perform access control and database optimization for albums."""
@@ -84,6 +79,11 @@ class GalleryIndexView(GalleryCommonMixin, AlbumListWithPreviewMixin, ArchiveInd
         if query:
             qs = qs.filter(name__contains=query)
         return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(GalleryIndexView, self).get_context_data(**kwargs)
+        context['title'] = getattr(settings, 'GALLERY_TITLE', u"Gallery")
+        return context
 
 
 class GalleryYearView(GalleryCommonMixin, AlbumListWithPreviewMixin, YearArchiveView):
@@ -158,7 +158,7 @@ def resized_photo(request, preset, pk):
     response = serve_private_media(request, path)
 
     root, ext = os.path.splitext(asciify(photo.filename))
-    width, height, _ = settings.PHOTO_RESIZE_PRESETS[preset]
+    width, height, _ = settings.GALLERY_RESIZE_PRESETS[preset]
     ascii_filename = '%s_%sx%s%s' % (root, width, height, ext)
     response['Content-Disposition'] = 'inline; filename=%s;' % ascii_filename
     return response
@@ -191,15 +191,16 @@ def serve_private_media(request, path):
     view. If ``DEBUG`` is ``False``, it sets a header and doesn't send the
     actual contents of the file.
 
-    The name of the header is defined by ``settings.SENDFILE_HEADER``. Use
-    ``'X-Accel-Redirect'`` for nginx and ``'X-SendFile'`` for Apache.
+    The name of the header is defined by ``settings.GALLERY_SENDFILE_HEADER``.
+    Use ``'X-Accel-Redirect'`` for nginx and ``'X-SendFile'`` for Apache.
 
     ``path`` must be an absolute path. Depending on your webserver's
     configuration, the header should contain either a relative path or full
-    path. Therefore, ``settings.SENDFILE_ROOT`` will be stripped from the
-    beginning of the path to create the header's value. It must be the root of
-    the internal location under nginx. It may be XSendFilePath or empty for
-    Apache.
+    path. Therefore, ``settings.GALLERY_SENDFILE_ROOT`` will be stripped from
+    the beginning of the path to create the header's value. It must be the
+    root of the internal location under nginx. It may be XSendFilePath or
+    empty for Apache.
+
     """
     if not os.path.exists(path):
         # Don't reveal the file name on the filesystem.
@@ -219,11 +220,11 @@ def serve_private_media(request, path):
             response = HttpResponse(f.read(), content_type=content_type)
     else:
         response = HttpResponse('', content_type=content_type)
-        if settings.SENDFILE_ROOT:
-            if not path.startswith(settings.SENDFILE_ROOT):
-                raise ValueError("Requested file isn't under SENDFILE_ROOT.")
-            path = path[len(settings.SENDFILE_ROOT):]
-        response[settings.SENDFILE_HEADER] = path.encode(sys.getfilesystemencoding())
+        if settings.GALLERY_SENDFILE_ROOT:
+            if not path.startswith(settings.GALLERY_SENDFILE_ROOT):
+                raise ValueError("Requested file isn't under GALLERY_SENDFILE_ROOT.")
+            path = path[len(settings.GALLERY_SENDFILE_ROOT):]
+        response[settings.GALLERY_SENDFILE_HEADER] = path.encode(sys.getfilesystemencoding())
 
     # resume copy-paste from django.views.static.serve
     response["Last-Modified"] = http_date(statobj.st_mtime)

gallery/tests/views.py
@@ -21,13 +21,13 @@ class ServePrivateMediaTests(TestCase):
 
     # See https://tn123.org/mod_xsendfile/
 
-    @override_settings(DEBUG=True, SENDFILE_HEADER='X-SendFile', SENDFILE_ROOT='')
+    @override_settings(DEBUG=True, GALLERY_SENDFILE_HEADER='X-SendFile', GALLERY_SENDFILE_ROOT='')
     def test_apache_dev(self):
         response = self.client.get(self.private_url)
         self.assertEqual(response.get('X-SendFile'), None)
         self.assertEqual(response.content, self.file_contents)
 
-    @override_settings(DEBUG=False, SENDFILE_HEADER='X-SendFile', SENDFILE_ROOT='')
+    @override_settings(DEBUG=False, GALLERY_SENDFILE_HEADER='X-SendFile', GALLERY_SENDFILE_ROOT='')
     def test_apache_prod(self):
         response = self.client.get(self.private_url)
         self.assertEqual(response.get('X-SendFile'), self.absolute_path)
@@ -35,13 +35,13 @@ class ServePrivateMediaTests(TestCase):
 
     # See http://wiki.nginx.org/XSendfile
 
-    @override_settings(DEBUG=True, SENDFILE_HEADER='X-Accel-Redirect', SENDFILE_ROOT=root_dir)
+    @override_settings(DEBUG=True, GALLERY_SENDFILE_HEADER='X-Accel-Redirect', GALLERY_SENDFILE_ROOT=root_dir)
     def test_nginx_dev(self):
         response = self.client.get(self.private_url)
         self.assertEqual(response.get('X-Accel-Redirect'), None)
         self.assertEqual(response.content, self.file_contents)
 
-    @override_settings(DEBUG=False, SENDFILE_HEADER='X-Accel-Redirect', SENDFILE_ROOT=root_dir)
+    @override_settings(DEBUG=False, GALLERY_SENDFILE_HEADER='X-Accel-Redirect', GALLERY_SENDFILE_ROOT=root_dir)
     def test_nginx_prod(self):
         response = self.client.get(self.private_url)
         self.assertEqual(response.get('X-Accel-Redirect'), self.relative_path)
@@ -54,6 +54,6 @@ class ServePrivateMediaTests(TestCase):
         response = self.client.get(self.private_url + '.does.not.exist')
         self.assertEqual(response.status_code, 404)
 
-    @override_settings(DEBUG=False, SENDFILE_HEADER='X-Accel-Redirect', SENDFILE_ROOT=root_dir + root_dir)
+    @override_settings(DEBUG=False, GALLERY_SENDFILE_HEADER='X-Accel-Redirect', GALLERY_SENDFILE_ROOT=root_dir + root_dir)
     def test_file_not_under_root(self):
         self.assertRaises(ValueError, self.client.get, self.private_url)

gallery/imgutil.py
@@ -44,7 +44,7 @@ def make_thumbnail(image_path, thumb_path, preset):
             pass
     # Pre-crop if requested and the aspect ratios don't match exactly
     image_width, image_height = image.size
-    thumb_width, thumb_height, crop = settings.PHOTO_RESIZE_PRESETS[preset]
+    thumb_width, thumb_height, crop = settings.GALLERY_RESIZE_PRESETS[preset]
     if crop:
         if thumb_width * image_height > image_width * thumb_height:
             target_height = image_width * thumb_height // thumb_width
@@ -56,7 +56,7 @@ def make_thumbnail(image_path, thumb_path, preset):
             image = image.crop((left, 0, left + target_width, image_height))
     # Save the thumbnail
     image.thumbnail((thumb_width, thumb_height), Image.ANTIALIAS)
-    options = settings.PHOTO_RESIZE_OPTIONS.get(image.format, {})
+    options = getattr(settings, 'GALLERY_RESIZE_OPTIONS', {}).get(image.format, {})
     try:
         if not os.path.isdir(os.path.dirname(thumb_path)):
             os.makedirs(os.path.dirname(thumb_path))

gallery/management/commands/scanphotos.py
@@ -20,14 +20,14 @@ from ...models import Album, Photo
 
 
 class Command(base.NoArgsCommand):
-    help = 'Scan PHOTO_ROOT and update database.'
+    help = 'Scan GALLERY_PHOTO_DIR and update database.'
 
     @transaction.commit_on_success
     def handle_noargs(self, **options):
         self.verbosity = int(options.get('verbosity', '1'))
         if self.verbosity >= 1:
             t = time.time()
-            self.stdout.write(u"Scanning %s\n" % settings.PHOTO_ROOT)
+            self.stdout.write(u"Scanning %s\n" % settings.GALLERY_PHOTO_DIR)
         albums = scan_photo_root(self)
         synchronize_albums(albums, self)
         synchronize_photos(albums, self)
@@ -36,13 +36,13 @@ class Command(base.NoArgsCommand):
             self.stdout.write(u"Done (%.02fs)\n" % dt)
 
 
-ignores = [re.compile(pat) for pat in settings.PHOTO_IGNORES]
+ignores = [re.compile(i) for i in getattr(settings, 'GALLERY_IGNORES', ())]
 
 def is_ignored(path):
     return any(pat.match(path) for pat in ignores)
 
 
-patterns = [(cat, re.compile(pat)) for cat, pat in settings.PHOTO_PATTERNS]
+patterns = [(cat, re.compile(pat)) for cat, pat in settings.GALLERY_PATTERNS]
 
 def is_matched(path):
     for category, pattern in patterns:
@@ -55,12 +55,12 @@ fs_encoding = sys.getfilesystemencoding()
 
 def iter_photo_root(command):
     """Yield relative path, category and regex captures for each photo."""
-    for dirpath, _, filenames in os.walk(settings.PHOTO_ROOT):
+    for dirpath, _, filenames in os.walk(settings.GALLERY_PHOTO_DIR):
         dirpath = dirpath.decode(fs_encoding)
         for filename in filenames:
             filename = filename.decode(fs_encoding)
             filepath = os.path.join(dirpath, filename)
-            relpath = os.path.relpath(filepath, settings.PHOTO_ROOT)
+            relpath = os.path.relpath(filepath, settings.GALLERY_PHOTO_DIR)
             if is_ignored(relpath):
                 if command.verbosity >= 3:
                     command.stdout.write(u"- %s\n" % relpath)

gallery/models.py
@@ -190,19 +190,19 @@ class Photo(models.Model):
         return photos.order_by('-date', '-filename')[:1].get()
 
     def abspath(self):
-        return os.path.join(settings.PHOTO_ROOT, self.album.dirpath, self.filename)
+        return os.path.join(settings.GALLERY_PHOTO_DIR, self.album.dirpath, self.filename)
 
     def thumbname(self, preset):
         prefix = self.album.date.strftime('%y%m')
         hsh = hashlib.sha1()
         hsh.update(self.album.dirpath.encode('utf-8'))
         hsh.update(self.filename.encode('utf-8'))
-        hsh.update(str(settings.PHOTO_RESIZE_PRESETS[preset]))
+        hsh.update(str(settings.GALLERY_RESIZE_PRESETS[preset]))
         ext = os.path.splitext(self.filename)[1].lower()
         return os.path.join(prefix, hsh.hexdigest() + ext)
 
     def thumbnail(self, preset):
-        thumbpath = os.path.join(settings.PHOTO_CACHE, self.thumbname(preset))
+        thumbpath = os.path.join(settings.GALLERY_CACHE_DIR, self.thumbname(preset))
         if not os.path.exists(thumbpath):
             make_thumbnail(self.abspath(), thumbpath, preset)
         return thumbpath

gallery/tests/admin.py
@@ -44,7 +44,7 @@ class AdminTests(TestCase):
         with open(os.path.join(tmpdir, 'test'), 'wb') as handle:
             handle.write('test')
         try:
-            with override_settings(PHOTO_ROOT=tmpdir):
+            with override_settings(GALLERY_PHOTO_DIR=tmpdir):
                 self.client.post(reverse('admin:gallery.admin.scan_photos'))
         finally:
             shutil.rmtree(tmpdir)

gallery/tests/settings.py
@@ -23,18 +23,14 @@ SECRET_KEY = 'Not empty for tests.'
 
 # Custom settings
 
-PHOTO_CACHE = ''
+GALLERY_CACHE_DIR = ''
 
-PHOTO_IGNORES = ()
+GALLERY_RESIZE_OPTIONS = {}
 
-PHOTO_PATTERNS = ()
+GALLERY_RESIZE_PRESETS = {}
 
-PHOTO_RESIZE_OPTIONS = {}
+GALLERY_PHOTO_DIR = ''
 
-PHOTO_RESIZE_PRESETS = {}
+GALLERY_SENDFILE_HEADER = 'X-SendFile'
 
-PHOTO_ROOT = ''
-
-SENDFILE_HEADER = 'X-SendFile'
-
-SENDFILE_ROOT = ''
+GALLERY_SENDFILE_ROOT = ''

commit 72564c14f09f66417979cce2599a6a238dd00397

Fix small regression in e82efd14b8.

gallery/views.py
@@ -66,6 +66,7 @@ class AlbumListWithPreviewMixin(AlbumListMixin):
                         photos.append(photo)
             album.photos_count = len(photos)
             album.preview = random.sample(photos, min(album.photos_count, 5))
+        context['title'] = getattr(settings, 'GALLERY_TITLE', u"Gallery")
         return context
 
 
@@ -79,11 +80,6 @@ class GalleryIndexView(GalleryCommonMixin, AlbumListWithPreviewMixin, ArchiveInd
         if query:
             qs = qs.filter(name__contains=query)
         return qs
-
-    def get_context_data(self, **kwargs):
-        context = super(GalleryIndexView, self).get_context_data(**kwargs)
-        context['title'] = getattr(settings, 'GALLERY_TITLE', u"Gallery")
-        return context
 
 
 class GalleryYearView(GalleryCommonMixin, AlbumListWithPreviewMixin, YearArchiveView):