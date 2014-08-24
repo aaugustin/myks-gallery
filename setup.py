6458071b981238b07933b7df9ca9b744da68a4e5
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-08-24 22:25:36 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(README) as f:
 
 distutils.core.setup(
     name='myks-gallery',
-    version='0.3',
+    version='0.4',
     author='Aymeric Augustin',
     author_email='aymeric.augustin@m4x.org',
     url='https://github.com/aaugustin/myks-gallery',
