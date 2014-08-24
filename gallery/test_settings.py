0dd7cfb55cd9a62486b0c84b329506b72c0a8ab8
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-08-24 22:19:22 +0200

    Adapt to new MIDDLEWARE_CLASSES default in Django 1.7.

--- gallery/test_settings.py
+++ gallery/test_settings.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {'ENGINE': 'django.db.backends.sqlite3'},
 }
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'gallery',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -14,7 +14,16 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
-)
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = 'gallery.test_urls'
 
