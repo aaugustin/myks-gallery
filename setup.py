807f0cabfc717bcdb84d7868fd9d49862c32e352
Aymeric Augustin <aymeric.augustin@m4x.org>
2014-09-14 13:54:40 +0200

    Python 3.4 is supported too.

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ distutils.core.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
     platforms='all',
     license='BSD'
