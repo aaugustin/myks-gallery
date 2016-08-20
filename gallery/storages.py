f89dce3ff6d0858c5a29b96610fe4113d6200184
Aymeric Augustin <aymeric.augustin@m4x.org>
2016-08-20 21:53:52 +0200

    Remove backwards compatibility with Django < 1.8.

--- gallery/storages.py
+++ gallery/storages.py
@@ -8,13 +8,9 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.files.storage import FileSystemStorage
 from django.dispatch import receiver
+from django.test.signals import setting_changed
 from django.utils.lru_cache import lru_cache
 from django.utils.module_loading import import_string
-
-try:
-    from django.test.signals import setting_changed         # Django ≥ 1.8
-except ImportError:                                         # pragma: no cover
-    from django.core.signals import setting_changed         # Django < 1.8
 
 
 @lru_cache()
